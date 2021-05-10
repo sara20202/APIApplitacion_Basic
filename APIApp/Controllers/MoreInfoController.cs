f7b6e1e3386258bba9ee309d967be4155923d24d
sara20202 <zhurisara@outlook.com>
2021-05-10 17:05:58 +0200

    some init msg

---
+++ APIApp/Controllers/MoreInfoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoreInfoController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Info1", "Info2","How Creative" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Your ID is:{id}";
+        }
+    }
+}
